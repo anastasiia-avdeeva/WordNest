fb82d3cb52d5e2139dc1140e4227193f6fea0afc
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-05-30 23:47:16 +0300

    add file with common const variables

---
+++ src/common/constants/constants.js
@@ -0,0 +1,46 @@
+export const words = [
+  {
+    id: 1,
+    word: "apple",
+    transcription: "['æp.əl]",
+    translation: "яблоко",
+    theme: "Еда",
+  },
+  {
+    id: 2,
+    word: "book",
+    transcription: "[bʊk]",
+    translation: "книга",
+    theme: "Учёба",
+  },
+  {
+    id: 3,
+    word: "dog",
+    transcription: "[dɒg]",
+    translation: "собака",
+    theme: "Животные",
+  },
+  {
+    id: 4,
+    word: "sun",
+    transcription: "[sʌn]",
+    translation: "солнце",
+    theme: "Природа",
+  },
+
+  {
+    id: 5,
+    word: "butterfly",
+    transcription: "[ˈbʌt.ə.flaɪ]",
+    translation: "бабочка",
+    theme: "Животные",
+  },
+];
+
+export const wordTableHeaderCells = [
+  "Cлово",
+  "Транскрипция",
+  "Перевод",
+  "Тема",
+  "Управление",
+];
