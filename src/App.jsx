9c712f08a1ab7483decf81505ac0ba3efe68b486
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-05-31 16:30:52 +0300

    create MainBlock component and use it in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { MainBlock } from "./components/mainBlock/MainBlock";
 import { SiteHeader } from "./components/siteHeader/SiteHeader";
 
 function App() {
   return (
     <>
       <SiteHeader />
+      <MainBlock />
     </>
   );
 }
