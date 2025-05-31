c55b549bdb7a67f14129371a78e6014cb6a29f5e
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-05-31 19:24:25 +0300

    create EditTableRow component and styles for it, use it WordTable component

--- src/components/wordTable/WordTable.jsx
+++ src/components/wordTable/WordTable.jsx
@@ -4,12 +4,12 @@ import {
 } from "../../common/constants/constants";
 import { HeaderRow } from "../headerRow/HeaderRow";
 import { WordTableRow } from "../wordTableRow/WordTableRow";
-// import { WordEditableRow } from "../WordEditableRow/WordEditableRow";
+import { EditTableRow } from "../editTableRow/EditTableRow";
 import { sortByName } from "../../common//utils/stringUtils";
 import styles from "./WordTable.module.scss";
 
 export const WordTable = () => {
-  //   const onEdit = 3;
+  const onEdit = 3;
   const sortedWords = sortByName(words);
   return (
     <table className={styles.table}>
@@ -17,12 +17,13 @@ export const WordTable = () => {
         <HeaderRow headerCells={WORD_TABLE_HEADER_CELLS} />
       </thead>
       <tbody className={styles.table__body}>
-        {sortedWords.map(({ id, ...rest }, index) => (
-          //   word.id === onEdit ? (
-          //     <WordEditableRow key={word.id} word={word} />
-          //   ) : (
-          <WordTableRow key={id} word={rest} index={index} />
-        ))}
+        {sortedWords.map(({ id, ...rest }, index) =>
+          id === onEdit ? (
+            <EditTableRow key={id} word={rest} index={index} />
+          ) : (
+            <WordTableRow key={id} word={rest} index={index} />
+          )
+        )}
       </tbody>
     </table>
   );
