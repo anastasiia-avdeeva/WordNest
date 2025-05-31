0eceacec701b4636e4f46bb0ee2dde71e677758c
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-05-31 14:58:19 +0300

    rename classes for textButton, create component HeaderRow for table and styles for it

--- src/components/textBtn/TextBtn.jsx
+++ src/components/textBtn/TextBtn.jsx
@@ -1,7 +1,7 @@
 import classNames from "classnames";
 import styles from "./TextBtn.module.scss";
 
-export const TextButton = ({ variant, children }) => {
+export const TextBtn = ({ variant, children }) => {
   const buttonClass = classNames(styles.button, { [styles[variant]]: variant });
   return <button className={buttonClass}>{children}</button>;
 };
