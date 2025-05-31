3e34fef0708f5e20fe7dc245e5e5b1f43e860e19
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-05-31 15:06:57 +0300

    create ActionsCell component and styles module, make TextBtn more flexible

---
+++ src/components/actionCell/ActionsCell.jsx
@@ -0,0 +1,16 @@
+import { TextBtn } from "../textBtn/TextBtn";
+import styles from "./ActionsCell.module.scss";
+
+export const ActionsCell = ({
+  btnVariant1,
+  btnVariant2,
+  btnText1,
+  btnText2,
+}) => {
+  return (
+    <div className={styles.table__btns}>
+      <TextBtn variant={btnVariant1}>{btnText1}</TextBtn>
+      <TextBtn variant={btnVariant2}>{btnText2}</TextBtn>
+    </div>
+  );
+};
--- src/components/textBtn/TextBtn.jsx
+++ src/components/textBtn/TextBtn.jsx
@@ -2,6 +2,6 @@ import classNames from "classnames";
 import styles from "./TextBtn.module.scss";
 
 export const TextBtn = ({ variant, children }) => {
-  const buttonClass = classNames(styles.button, { [styles[variant]]: variant });
+  const buttonClass = classNames(styles.button, variant && styles[variant]);
   return <button className={buttonClass}>{children}</button>;
 };
