ce6eac56ff181c6c9ee08ec07d445eab570bba0a
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-05-31 15:00:33 +0300

    change logoClass initiation

--- src/components/siteLogo/SiteLogo.jsx
+++ src/components/siteLogo/SiteLogo.jsx
@@ -2,7 +2,7 @@ import classNames from "classnames";
 import styles from "./SiteLogo.module.scss";
 
 export const SiteLogo = ({ variant, icon }) => {
-  const logoClass = classNames(styles.logo, { [styles[variant]]: variant });
+  const logoClass = classNames(styles.logo, variant && styles[variant]);
   return (
     <div className={logoClass}>
       <a href="/" className={styles.logo__link}>
