fe3cbec0eaedc48bfd8a083069a6aedc37bb0c6e
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-06-21 03:11:40 +0300

    import framer-motion to animate crads slider, use it in WordCards component, move inline styles to WordCards.module.scss

--- src/components/WordCards/WordCards.jsx
+++ src/components/WordCards/WordCards.jsx
@@ -1,12 +1,34 @@
 import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion"; // обязательно из 'framer-motion'
 import { WordCard } from "./WordCard/WordCard";
 import styles from "./WordCards.module.scss";
 import { IconBtn } from "../UI/IconBtn/IconBtn";
 import nextIcon from "../../assets/icons/nextIcon.svg";
 import prevIcon from "../../assets/icons/prevIcon.svg";
 
+const variants = {
+  enter: (direction) => ({
+    x: direction > 0 ? 300 : -300,
+    opacity: 0,
+    position: "absolute",
+  }),
+  center: {
+    x: 0,
+    opacity: 1,
+    position: "relative",
+  },
+  exit: (direction) => ({
+    x: direction > 0 ? -300 : 300,
+    opacity: 0,
+    position: "absolute",
+  }),
+};
+
 export const WordCards = ({ words = [], initialIndex = 0 }) => {
-  const [currentIndex, setCurrentIndex] = useState(initialIndex);
+  const [[currentIndex, direction], setCurrentIndex] = useState([
+    initialIndex,
+    0,
+  ]);
 
   if (!words.length) {
     return (
@@ -16,24 +38,36 @@ export const WordCards = ({ words = [], initialIndex = 0 }) => {
     );
   }
 
-  const handleNext = () => {
-    setCurrentIndex((previousIndex) => (previousIndex + 1) % words.length);
+  const paginate = (newDirection) => {
+    setCurrentIndex(([prevIndex]) => {
+      const newIndex = (prevIndex + newDirection + words.length) % words.length;
+      return [newIndex, newDirection];
+    });
   };
-  const handlePrevious = () => {
-    setCurrentIndex(
-      (previousIndex) => (previousIndex - 1 + words.length) % words.length
-    );
-  };
+
   return (
     <div className={styles.cards}>
       <IconBtn
         variant="slider-button"
         icon={prevIcon}
         alt="кнопка назад"
-        onClick={handlePrevious}
+        onClick={() => paginate(-1)}
       />
       <div className={styles["cards__card-and-counter"]}>
-        <WordCard key={currentIndex} word={words[currentIndex]} />
+        <AnimatePresence custom={direction} initial={false}>
+          <motion.div
+            key={currentIndex}
+            custom={direction}
+            variants={variants}
+            initial="enter"
+            animate="center"
+            exit="exit"
+            transition={{ type: "tween", duration: 0.4 }}
+            className={styles["cards__motion-wrapper"]}
+          >
+            <WordCard word={words[currentIndex]} />
+          </motion.div>
+        </AnimatePresence>
         <p className={styles.cards__counter}>{`${currentIndex + 1}/${
           words.length
         }`}</p>
@@ -42,7 +76,7 @@ export const WordCards = ({ words = [], initialIndex = 0 }) => {
         variant="slider-button"
         icon={nextIcon}
         alt="кнопка вперед"
-        onClick={handleNext}
+        onClick={() => paginate(1)}
       />
     </div>
   );
